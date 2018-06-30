4288d9f2c995562d3ca97308531410f890c75cad
二撞 宋 <2627660832@qq.com>
2018-06-30 20:58:33 +0800

    ..

---
+++ Com.Dave.ProtocolHelper/TiltSensor/Common/CommonEnum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TiltSensor.Common
+{
+    public class CommonEnum
+    {
+        public enum SerialPortOperationState
+        {
+            None,
+            Send,
+            Receive
+        }
+        public enum SerialPortOperationResult
+        {
+            None,
+            Succeessful,
+            Failed
+        }
+    }
+}
--- Com.Dave.ProtocolHelper/TiltSensor/TiltSensor_ACA826T.cs
+++ Com.Dave.ProtocolHelper/TiltSensor/TiltSensor_ACA826T.cs
@@ -4,25 +4,39 @@ using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading;
+using static TiltSensor.Common.CommonEnum;
 
 namespace TiltSensor
 {
     /// <summary>
     /// ACA826T-全温补高精度数字输出型双轴倾角传感器
     /// </summary>
-    public class TiltSensor_ACA826T
+    public class TiltSensor_ACA826T:IDisposable
     {
         private byte _frameIdentifier = 0x68;
+        /// <summary>
+        /// 
+        /// </summary>
+        private SerialPortOperationState _portOperationState = SerialPortOperationState.None;
+        /// <summary>
+        /// 
+        /// </summary>
+        private SerialPortOperationResult _portOperatingResult = SerialPortOperationResult.None;
         private SerialPort _port = new SerialPort() { BaudRate = 9600, StopBits = StopBits.One, DataBits = 8, Parity = Parity.None, PortName = "COM3" };
-        public TiltSensor_ACA826T()
+        public TiltSensor_ACA826T(string portName)
         {
-            var ss = SerialPort.GetPortNames();
-            Port.Open();
+            _port.PortName = portName;      
+                     
+        }
+        public void StartCollectData()
+        {
             flag = true;
             Thread thread = new Thread(ThreadSendAndReceive) { IsBackground = true };
             thread.Start();
-            // Port.DataReceived += _port_DataReceived;
         }
+        /// <summary>
+        /// 
+        /// </summary>
         public bool flag = false;
         private int _offset = 0;
         private int _portReceivedTimeOutCount = 0;
@@ -39,45 +53,65 @@ namespace TiltSensor
                 _port = value;
             }
         }
-
-        public void ThreadSendAndReceive()
+        private void SerialPortSend()
         {
-            while (flag)
+
+        }
+        private void SerialPortReceive()
+        {
+            if (Port.BytesToRead > 0)
             {
-                Thread.Sleep(20);
-                if (Port.BytesToRead > 0)
+                if (Port.BytesToRead != _offset)
                 {
-                    if (Port.BytesToRead != _offset)
+                    _offset = Port.BytesToRead;
+                    _portReceivedTimeOutCount = 0;
+                }
+                else
+                {
+                    _portReceivedTimeOutCount++;
+                    if (_portReceivedTimeOutCount > 5)
                     {
-                        _offset = Port.BytesToRead;
                         _portReceivedTimeOutCount = 0;
-                    }
-                    else
-                    {
-                        _portReceivedTimeOutCount++;
-                        if (_portReceivedTimeOutCount > 5)
+                        byte[] buffers = new byte[_offset];
+                        _port.Read(buffers, 0, _offset);
+                        Console.WriteLine("\r\n{0:D2}s{1:D3}ms Data Received:", DateTime.Now.Second, DateTime.Now.Millisecond);
+                        foreach (byte item in buffers)
                         {
-                            _portReceivedTimeOutCount = 0;
-                            byte[] buffers = new byte[_offset];
-                            _port.Read(buffers, 0, _offset);
-                            Console.WriteLine("\r\n{0:D2}s{1:D3}ms Data Received:", DateTime.Now.Second,DateTime.Now.Millisecond);
-                            foreach (byte item in buffers)
-                            {
-                                Console.Write(item.ToString("X2")+" ");
-                            }
-                            Console.WriteLine("\r\nlength:"+buffers.Length);
-                            _offset = 0;
+                            Console.Write(item.ToString("X2") + " ");
                         }
+                        Console.WriteLine("\r\nlength:" + buffers.Length);
+                        _offset = 0;
                     }
                 }
             }
         }
-        private void _port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        /// <summary>
+        /// 串口发送接收
+        /// </summary>
+        public void ThreadSendAndReceive()
         {
-            SerialPort sp = (SerialPort)sender;
-            string indata = sp.ReadExisting();
-            Console.WriteLine("\r\nData Received:");
-            Console.Write(indata);
+            if (!_port.IsOpen)
+            {
+                Port.Open();
+            }           
+            while (flag)
+            {
+                Thread.Sleep(10);
+
+               
+            }
+            flag = false;
+            if (_port.IsOpen)
+            {
+                Port.Close();
+            }
+        }
+
+        void Dispose()
+        {
+            flag = false;
+            _port.Dispose();
+
         }
     }
 }
