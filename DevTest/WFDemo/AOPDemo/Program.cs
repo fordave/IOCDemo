158c4c96ad42666cf3dd92c0a02a0e8bff08a123
fordave <2627660832@qq.com>
2018-08-17 16:53:40 +0800

    add aop test

---
+++ DevTest/WFDemo/AOPDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using AOPDemo.Entities;
+using AOPDemo.Impl;
+using AOPDemo.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+
+namespace AOPDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***\r\n Begin program - no logging\r\n");
+            //        IRepository<Customer> customerRepository =
+            //new LoggerRepository<Customer>(new Repository<Customer>());
+            Console.WriteLine("\r\nRunning as admin");
+            Thread.CurrentPrincipal =
+              new GenericPrincipal(new GenericIdentity("Administrator"),
+              new[] { "ADMIN" });
+            IRepository<Customer> customerRepository =
+     RepositoryFactory.Create<Customer>();
+            var customer = new Customer
+            {
+                Id = 1,
+                Name = "Customer 1",
+                Address = "Address 1"
+            };
+            customerRepository.Add(customer);
+            customerRepository.Update(customer);
+            customerRepository.Delete(customer);
+            Console.WriteLine("\r\nRunning as user");
+            Thread.CurrentPrincipal =
+              new GenericPrincipal(new GenericIdentity("NormalUser"),
+              new string[] { });
+            customerRepository.Add(customer);
+            customerRepository.Update(customer);
+            customerRepository.Delete(customer);
+            Console.WriteLine(
+              "\r\nEnd program - logging and authentication\r\n***");
+            Console.WriteLine("\r\nEnd program - no logging\r\n***");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DevTest/WFDemo/AOPDemo/RepositoryFactory.cs
@@ -0,0 +1,25 @@
+﻿using AOPDemo.Impl;
+using AOPDemo.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AOPDemo
+{
+    public class RepositoryFactory
+    {
+        public static IRepository<T> Create<T>()
+        {
+            var repository = new Repository<T>();
+            var decoratedRepository =
+              (IRepository<T>)new DynamicProxy<IRepository<T>>(
+              repository).GetTransparentProxy();
+            // Create a dynamic proxy for the class already decorated
+            decoratedRepository =
+              (IRepository<T>)new AuthenticationProxy<IRepository<T>>(
+              decoratedRepository).GetTransparentProxy();
+            return decoratedRepository;
+        }
+    }
+}
