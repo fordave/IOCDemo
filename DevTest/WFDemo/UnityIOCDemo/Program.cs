170bcdb4822bf07ac767a008f0327c00f45dc4b8
fordave <2627660832@qq.com>
2018-08-20 09:30:48 +0800

    ..

---
+++ DevTest/WFDemo/UnityIOCDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace UnityIOCDemo
+{
+    class Program
+    {
+        private static IUnityContainer container = null;
+        static void Main(string[] args)
+        {
+            RegisterContainer();
+
+            var programmer = container.Resolve<IProgrammer>("CSharp");
+            var programmer1 = container.Resolve<IProgrammer>("VB");
+            var programmer2 = container.Resolve<IProgrammer>("Java");
+            programmer.Working();
+            programmer1.Working();
+            programmer2.Working();
+
+            Console.ReadKey();
+        }
+        private static void RegisterContainer()
+        {
+            container = new UnityContainer();
+            UnityConfigurationSection config = (UnityConfigurationSection)ConfigurationManager.GetSection(UnityConfigurationSection.SectionName);
+            config.Configure(container, "Programmer");
+        }
+    }
+}
